4e0c9b834e0e6e88414a6cb18b5fe635ef011982
Ivan MSI <arigatory@gmail.com>
2022-02-20 17:28:48 +0300

    Writing the Application Logic in the Request Handler

---
+++ GloboTicket.TicketManagement.Application/Features/Events/GetEventsListQuery.cs
@@ -0,0 +1,10 @@
+﻿using MediatR;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GloboTicket.TicketManagement.Application.Features.Events
+{
+    public class GetEventsListQuery : IRequest<List<EventListVm>>
+    {
+    }
+}
---
+++ GloboTicket.TicketManagement.Application/Features/Profiles/MappingProfile.cs
@@ -0,0 +1,16 @@
+﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Features.Events;
+using GloboTicket.TicketManagement.Domain.Entities;
+
+namespace GloboTicket.TicketManagement.Application.Features.Profiles
+{
+    public class MappingProfile : Profile
+    {
+        public MappingProfile()
+        {
+            CreateMap<Event, EventListVm>().ReverseMap();
+            CreateMap<Event, EventDetailVm>().ReverseMap();
+            CreateMap<Category, CategoryDto>();
+        }
+    }
+}
