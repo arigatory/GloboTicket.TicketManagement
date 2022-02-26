6bfe24ed3f3cfb6ed86e7751ef4d6af630c1f126
Ivan MSI <arigatory@gmail.com>
2022-02-26 23:17:35 +0300

    my done

--- GloboTicket.TicketManagement.Api/Startup.cs
+++ GloboTicket.TicketManagement.Api/Startup.cs
@@ -1,16 +1,13 @@
+using GloboTicket.TicketManagement.Api.Utility;
 using GloboTicket.TicketManagement.Application;
 using GloboTicket.TicketManagement.Infrastructure;
 using GloboTicket.TicketManagement.Persistence;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using Microsoft.OpenApi.Models;
 
 namespace GloboTicket.TicketManagement.Api
 {
@@ -37,6 +34,20 @@ namespace GloboTicket.TicketManagement.Api
             });
         }
 
+        private void AddSwagger(IServiceCollection services)
+        {
+            services.AddSwaggerGen(c =>
+            {
+                c.SwaggerDoc("v1", new OpenApiInfo
+                {
+                    Version = "v1",
+                    Title = "GloboTicket Ticket Management API"
+                });
+
+                c.OperationFilter<FileResultContentTypeOperationFilter>();
+            });
+        }
+
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
@@ -46,6 +57,12 @@ namespace GloboTicket.TicketManagement.Api
             }
             app.UseHttpsRedirection();
             app.UseRouting();
+
+            app.UseSwagger();
+            app.UseSwaggerUI(c =>
+            {
+                c.SwaggerEndpoint("/swagger/v1/swagger.json", "GloboTicket Ticket Management API");
+            });
 
             app.UseCors("Open");
 
