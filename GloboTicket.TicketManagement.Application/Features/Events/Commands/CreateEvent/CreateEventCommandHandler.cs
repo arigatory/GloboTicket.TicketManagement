030bab62dd331faced5abff8f8f0cb485be0ef8b
Ivan MSI <arigatory@gmail.com>
2023-02-01 23:09:27 +0300

    add presistence

--- GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
+++ GloboTicket.TicketManagement.Application/Features/Events/Commands/CreateEvent/CreateEventCommandHandler.cs
@@ -1,36 +1,57 @@
 ﻿using AutoMapper;
+using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
 using GloboTicket.TicketManagement.Application.Contracts.Persistence;
+using GloboTicket.TicketManagement.Application.Models.Mail;
 using GloboTicket.TicketManagement.Domain.Entities;
 using MediatR;
 
-namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent
+namespace GloboTicket.TicketManagement.Application.Features.Events.Commands.CreateEvent;
+
+public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Guid>
 {
-    public class CreateEventCommandHandler : IRequestHandler<CreateEventCommand, Guid>
+    private readonly IEventRepository _eventRepository;
+    private readonly IMapper _mapper;
+    private readonly IEmailService _emailService;
+
+
+    public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository, IEmailService emailService)
     {
-        private readonly IEventRepository _eventRepository;
-        private readonly IMapper _mapper;
+        _mapper = mapper;
+        _eventRepository = eventRepository;
+        _emailService = emailService;
+    }
+
+    public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
+    {
+        var validator = new CreateEventCommandValidator(_eventRepository);
+        var validationResult = await validator.ValidateAsync(request);
+
+        if (validationResult.Errors.Count > 0)
+            throw new Exceptions.ValidationException(validationResult);
+
+        var @event = _mapper.Map<Event>(request);
 
 
-        public CreateEventCommandHandler(IMapper mapper, IEventRepository eventRepository)
+        @event = await _eventRepository.AddAsync(@event);
+
+
+        //Sending email notification to admin address
+        var email = new Email()
         {
-            _mapper = mapper;
-            _eventRepository = eventRepository;
+            To = "ivan@test.com",
+            Body = $"A new event was created: {request}",
+            Subject = "A new event was created"
+        };
+
+        try
+        {
+            await _emailService.SendEmail(email);
+        }
+        catch (Exception ex)
+        {
+            //this shouldn't stop the API from doing else so this can be logged
         }
 
-        public async Task<Guid> Handle(CreateEventCommand request, CancellationToken cancellationToken)
-        {
-            var validator = new CreateEventCommandValidator(_eventRepository);
-            var validationResult = await validator.ValidateAsync(request);
-
-            if (validationResult.Errors.Count > 0)
-                throw new Exceptions.ValidationException(validationResult);
-
-            var @event = _mapper.Map<Event>(request);
-
-
-            @event = await _eventRepository.AddAsync(@event);
-
-            return @event.EventId;
-        }
+        return @event.EventId;
     }
 }
