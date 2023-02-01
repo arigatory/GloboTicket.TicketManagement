2bee8a3d783a7e741547fe527ae2dba8e2b336e9
Ivan MSI <arigatory@gmail.com>
2023-02-01 23:54:18 +0300

    add sendgrid

--- GloboTicket.TicketManagement.Infrastructure/Mail/EmailService.cs
+++ GloboTicket.TicketManagement.Infrastructure/Mail/EmailService.cs
@@ -1,39 +1,41 @@
-﻿using GloboTicket.TicketManagement.Application.Models.Mail;
+﻿using GloboTicket.TicketManagement.Application.Contracts.Infrastructure;
+using GloboTicket.TicketManagement.Application.Models.Mail;
+using Microsoft.Extensions.Options;
 using SendGrid;
+using SendGrid.Helpers.Mail;
 
-namespace GloboTicket.TicketManagement.Infrastructure.Mail
+namespace GloboTicket.TicketManagement.Infrastructure.Mail;
+
+public class EmailService : IEmailService
 {
-    public class EmailService : IEmailService
+    public EmailSettings _emailSettings { get; }
+
+    public EmailService(IOptions<EmailSettings> mailSettings)
     {
-        public EmailSettings _emailSettings { get; }
+        _emailSettings = mailSettings.Value;
+    }
 
-        public EmailService(IOptions<EmailSettings> mailSettings)
+    public async Task<bool> SendEmail(Email email)
+    {
+        var client = new SendGridClient(_emailSettings.ApiKey);
+
+        var subject = email.Subject;
+        var to = new EmailAddress(email.To);
+        var emailBody = email.Body;
+
+        var from = new EmailAddress
         {
-            _emailSettings = mailSettings.Value;
-        }
+            Email = _emailSettings.FromAddress,
+            Name = _emailSettings.FromName
+        };
 
-        public async Task<bool> SendEmail(Email email)
-        {
-            var client = new SendGridClient(_emailSettings.ApiKey);
-
-            var subject = email.Subject;
-            var to = new EmailAddress(email.To);
-            var emailBody = email.Body;
-
-            var from = new EmailAddress
-            {
-                Email = _emailSettings.FromAddress,
-                Name = _emailSettings.FromName
-            };
-
-            var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
-            var response = await client.SendEmailAsync(sendGridMessage);
+        var sendGridMessage = MailHelper.CreateSingleEmail(from, to, subject, emailBody, emailBody);
+        var response = await client.SendEmailAsync(sendGridMessage);
 
 
-            if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
-                return true;
+        if (response.StatusCode == System.Net.HttpStatusCode.Accepted || response.StatusCode == System.Net.HttpStatusCode.OK)
+            return true;
 
-            return false;
-        }
+        return false;
     }
 }
